84ad0ffd4bd92b1cdccffcb49526180ee3e68929
abdulaziz5705 <abdulazizsukurov681@gmail.com>
2024-12-14 06:48:18 +0500

    .

--- test.py
+++ test.py
@@ -1,13 +1,2 @@
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        negative = x < 0
-        x = abs(x)
-        x = int(str(x)[::-1])
-        if negative:
-            x = -x
-            if x < (x % 10) * 100 + (x // 10) % 10 * 10 + x // 100 or x > (
-                    (x % 10) * 100 + (x // 10) % 10 * 10 + x // 100):
-                return False
-        return True
 
 
